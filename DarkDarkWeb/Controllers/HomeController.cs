ac98487de620fcdb510cd5a114d6e1b8ad2c5aae
Artur Shestakov <shestakovartur2.0@gmail.com>
2018-12-11 13:13:55 +0500

    search algorithm broken

--- DarkDarkWeb/Controllers/HomeController.cs
+++ DarkDarkWeb/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DDW.Core.Entities;
+using DDW.Core.ViewModels;
+using DDW.DataAccess.SQL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,16 +11,35 @@ namespace DarkDarkWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private DataContext db = new DataContext();
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult Search()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string keywordsString)
         {
-            ViewBag.Message = "Your application description page.";
+            return RedirectToAction("Search","Home", new {keyword = keywordsString });
+        }
+        public ActionResult Search(string keywordsString)
+        {
+            List<Resource> resources;
+            List<Category> categories = db.Categories.ToList();
+            if (keywordsString == null)
+            {
+                resources = db.Resources.ToList();
+            }
+            else
+            {
+               
+                resources = db.Resources.ToList().Search(keywordsString);
+            }
+            ResourceCategoriesListViewt model = new ResourceCategoriesListViewt();
+            model.Categories = categories;
+            model.Resources = resources;
+            return View(model);
 
-            return View();
         }
 
     }
--- DarkDarkWeb/Extencions.cs
+++ DarkDarkWeb/Extencions.cs
@@ -18,11 +18,39 @@ namespace DarkDarkWeb
             return keywordsCollection;
         }
 
+        public static string[] GetKeywordsString(this string keywords)
+        {
+            string[] stringSeparators = new string[] { ",", ";", ".", ":", "-", "|", " " };
+            return  keywords.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+        }
+
         public static string ToMonthName(this DateTime dateTime)
         {
             CultureInfo ci = new CultureInfo("en-US");
             return DateTime.Now.ToString("MMMM", ci);
-            return CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(dateTime.Month);
+        }
+
+        public static List<Resource> Search(this List<Resource> resources, string keywordsString)
+        {
+            var searchedKeywords = keywordsString.GetKeywordsString();
+            var foundresources = new List<Resource>();
+            for (int i = keywordsString.Count(); i>0; i--)
+            {
+                foreach(var r in resources)
+                {
+                    if (r.Keywords.HowManyKeywords(searchedKeywords) == i) foundresources.Add(r);
+                }
+            }
+            return resources;
+        }
+        private static int HowManyKeywords(this IEnumerable<Keyword> containedKeywords, string[] searchedKeywords)
+        {
+            int i = 0;
+            foreach(var word in searchedKeywords)
+            {
+                if (containedKeywords.Select(k => k.KeywordName).ToList().Contains(word)) i++;
+            }
+            return i;
         }
     }
 }
